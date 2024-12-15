5f4d4ace4cb24cd919a11400abe1b953f693f48c
Binu_Tamang <bunutmg323@gmail.com>
2024-12-15 19:07:40 +0545

    chore: initial project setup with Vite and ESLint configs

---
+++ src/assets/index.jsx
@@ -0,0 +1,29 @@
+import mainBanner from "./images/Backgroung-Image.png";
+import bannerBg from "./images/Background-Gradient.png";
+import PanditImg from "./images/panditji.jpg";
+import VastuDetectionImg from "./images/VastuDetection.jpg";
+import VenueBookingImg from "./images/VenueBooking.png";
+import PannaImg from "./images/Panna.jpg";
+import PushparajImg from "./images/Pushparaj.jpg";
+import RudrakshImg from "./images/Rudraksh.jpg";
+import HandImg from "./images/hand.png";
+import AboutImg from "./images/about.png";
+import TestimonialImg from "./assets/images/testimonialBg.png";
+import DiceImg from "./assets/images/Dice.png";
+
+export {
+    // eslint-disable-next-line react-refresh/only-export-components
+    mainBanner,
+    // eslint-disable-next-line react-refresh/only-export-components
+    bannerBg,
+    PanditImg,
+    VastuDetectionImg,
+    VenueBookingImg,
+    PannaImg,
+    PushparajImg,
+    RudrakshImg,
+    HandImg,
+    AboutImg,
+    DiceImg,
+    TestimonialImg
+};
